b9828b0071cbc8678c88e1a20c68b252be44562d
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-22 01:21:12 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,5 +3,9 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
+  attr_accessor :emails
 
+  def initialize(emails)
+    @emails = emails
+  end
 end
